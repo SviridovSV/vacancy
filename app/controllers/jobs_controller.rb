535174c075dbc2fafea1e6b451db97dba9fdc854
Sviridov Sergey <sviridov.sv82@gmail.com>
2016-08-19 16:38:33 +0300

    Public pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,28 @@
 class JobsController < ApplicationController
+	def index
+		@company = Company.find(params[:company_id])
+		@job = @company.jobs.all
+	end
+
+	def show
+		
+		@company = Company.find(params[:company_id])
+	    @job = @company.jobs.find(job_params)
+	end
+
+	def new
+	end
+
+	def create
+	  @company = Company.find(params[:company_id])
+	  @job = @company.jobs.create(job_params)
+
+	  
+	  redirect_to company_path(@company)
+	end
+
+	private
+		def job_params
+			params.require(:job).permit(:profession, :country, :city)
+		end
 end
